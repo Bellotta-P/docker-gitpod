4ab4c466330d606a53df71907b60c2ad7e8243c7
Bellotta-P <bellotta.pietro@iisgalvanimi.edu.it>
2023-02-10 11:14:50 +0000

    pietoto

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrlServer: 'https://8080-maliziag-dockergitpod-8yybykauofo.ws-eu38.gitpod.io/'
 };
 
 /*
